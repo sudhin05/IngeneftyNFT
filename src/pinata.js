81c77cf3bedd8fadab55a961b45eb17ddf3615ca
Sudhindra Pai <144920228+sudhin05@users.noreply.github.com>
2023-11-05 05:07:20 +0530

    Update pinata.js

--- src/pinata.js
+++ src/pinata.js
@@ -69,7 +69,7 @@ export const uploadFileToIPFS = async(file) => {
             maxBodyLength: 'Infinity',
             headers: {
                 'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-                pinata_api_key: key,
+                pinata_api_key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJiYTM4MGYzYy04NDg3LTQ0MmMtOTFhNS04M2E1ZGFiMWFmNTciLCJlbWFpbCI6ImlzcGljZXl4QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI1MTI0OTRkN2YwYzk2ODkzYzgxOCIsInNjb3BlZEtleVNlY3JldCI6ImIzYjJhODBkYTRkYTRmODA1ZDQwY2E0MDg2NDUyZGFiYjU0MzYxOGUxODNlNGQ3MDVkZTg1MTMwYmJlY2NjMDUiLCJpYXQiOjE2OTkwNzU3ODR9.qVpCXfIqsgKYaXHOY3aai45fR3rvOZwm4BknHelPrQ0,
                 pinata_secret_api_key: secret,
             }
         })
